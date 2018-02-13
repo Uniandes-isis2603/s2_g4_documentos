cf0c4b9cc5496ea90910021dc49856630c125cfc
federico7marroquin <32007880+federico7marroquin@users.noreply.github.com>
2018-02-12 23:56:18 -0500

    terminado
    terminado

---
+++ documentos-web/src/main/java/co/edu/uniandes/csw/documentos/dtos/DeseadoDetailedDTO.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.documentos.dtos;
+
+/**
+ *
+ * @author f.marroquin10
+ */
+public class DeseadoDetailedDTO {
+    
+    
+    /**
+     * constructor por defecto
+     */
+    public DeseadoDetailedDTO()
+    {
+        
+    }
+}
