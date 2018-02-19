065266aa25bac2db53bc22767cf8690a5424fe92
N-Sotelo98 <35941294+N-Sotelo98@users.noreply.github.com>
2018-02-19 14:31:26 -0500

    Test implemenatdos

---
+++ documentos-back/src/main/java/co/edu/uniandes/csw/documentos/entities/ComentarioEntity.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.documentos.entities;
+
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author n.sotelo
+ */
+@Entity
+public class ComentarioEntity 
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
+    
+    private Long id;
+    private String comentario;
+    private Date fecha;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    
+    private Integer calificacion;
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Integer getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(Integer calificacion) {
+        this.calificacion = calificacion;
+    }
+    
+   
+    
+}
