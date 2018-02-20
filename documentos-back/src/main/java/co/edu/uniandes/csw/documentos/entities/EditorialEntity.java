6a154bbff4ec731256547e13cc60683afbf3cb39
vianaernesto <30883023+vianaernesto@users.noreply.github.com>
2018-02-19 21:58:27 -0500

    Update EditorialEntity.java

--- documentos-back/src/main/java/co/edu/uniandes/csw/documentos/entities/EditorialEntity.java
+++ documentos-back/src/main/java/co/edu/uniandes/csw/documentos/entities/EditorialEntity.java
@@ -9,13 +9,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.io.Serializable;
 
 /**
  *
  * @author n.sotelo
  */
 @Entity
-public class EditorialEntity {
+public class EditorialEntity implements Serializable{
     
     @Id
      @GeneratedValue(strategy = GenerationType.IDENTITY)
