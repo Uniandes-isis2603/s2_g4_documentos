2a3752b3745959183231f32aeb4b57196e87abf7
federico7marroquin <32007880+federico7marroquin@users.noreply.github.com>
2018-02-28 23:53:15 -0500

    lógica 2.1.4

---
+++ documentos-back/src/test/java/co/edu/uniandes/csw/documentos/test/logic/DeseadoLogicTest.java
@@ -0,0 +1,15 @@
+package co.edu.uniandes.csw.documentos.test.logic;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author f.marroquin10
+ */
+public class DeseadoLogicTest {
+    
+}
