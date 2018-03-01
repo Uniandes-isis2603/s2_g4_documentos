8ce8a814bfedd767ca30ab41a2ea30a52779d9a3
jc.jaramillo10 <jc.jaramillo10@ISIS2603S2-0072.sis.virtual.uniandes.edu.co>
2018-02-28 21:58:20 -0500

    Se terminaron las reglas de negocio en cada una de las clases de logica

--- documentos-back/src/main/java/co/edu/uniandes/csw/documentos/ejb/AutorLogic.java
+++ documentos-back/src/main/java/co/edu/uniandes/csw/documentos/ejb/AutorLogic.java
@@ -7,6 +7,7 @@ package co.edu.uniandes.csw.documentos.ejb;
 
 import co.edu.uniandes.csw.documentos.entities.AutorEntity;
 import co.edu.uniandes.csw.documentos.persistence.AutorPersistence;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.Stateless;
@@ -49,4 +50,67 @@ public class AutorLogic {
         
         return null;
     }
+    
+     /**
+     * Obtiene la lista de los registros de Autor.
+     *
+     * @return Colección de objetos de AutorEntity.
+     */
+    public List<AutorEntity> getAutores() {
+        LOGGER.log(Level.INFO, "Inicia proceso de consultar todos los autores");
+        return persistence.findAll();
+    }
+    
+     /**
+     * Obtiene los datos de una instancia de Autor a partir de su ID.
+     *
+     * @param id Identificador de la instancia a consultar
+     * @return Instancia de AutorEntity con los datos del Autor consultado.
+     */
+    public AutorEntity getAutor(Long id) {
+        LOGGER.log(Level.INFO, "Inicia proceso de consultar un autor con id = {0}", id);
+        return persistence.find(id);
+    }
+
+    /**
+     * Actualiza la información de una instancia de Autor.
+     *
+     * @param entity Instancia de AutorEntity con los nuevos datos.
+     * @return Instancia de AuthorEntity con los datos actualizados.
+     */
+    public AutorEntity updateAutor(AutorEntity entity) {
+        LOGGER.log(Level.INFO, "Inicia proceso de actualizar un autor ");
+        
+        AutorEntity buscado = persistence.find(entity.getId());
+        if (buscado != null){
+            LOGGER.log(Level.INFO, "El autor con el id {0} no existe ", entity.getId());
+        }
+        else if (!entity.getNombre().matches("([A-Z]|[a-z]|\\s)+")){
+            LOGGER.log(Level.INFO, "El nombre del autor no puede contener caracteres especiales");
+        }
+        else{
+             return persistence.update(entity);
+         }
+        
+        return null;
+    }
+
+    /**
+     * Elimina una instancia de Autor de la base de datos.
+     *
+     * @param id Identificador de la instancia a eliminar.
+     */
+    public void deleteAutor(Long id) {
+        LOGGER.log(Level.INFO, "Inicia proceso de borrar un autor ");
+        
+        AutorEntity buscado = persistence.find(id);
+        if (buscado != null){
+            LOGGER.log(Level.INFO, "El autor con el id {0} no existe ", id);
+        }
+
+        else{
+             persistence.delete(id);
+         }
+    }
+
 }
--- documentos-back/src/main/java/co/edu/uniandes/csw/documentos/ejb/ImagenLogic.java
+++ documentos-back/src/main/java/co/edu/uniandes/csw/documentos/ejb/ImagenLogic.java
@@ -5,10 +5,12 @@
  */
 package co.edu.uniandes.csw.documentos.ejb;
 
+import co.edu.uniandes.csw.documentos.entities.AreaDeConocimientoEntity;
 import co.edu.uniandes.csw.documentos.entities.AutorEntity;
 import co.edu.uniandes.csw.documentos.entities.ImagenEntity;
 import co.edu.uniandes.csw.documentos.persistence.AutorPersistence;
 import co.edu.uniandes.csw.documentos.persistence.ImagenPersistence;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.Stateless;
@@ -16,7 +18,7 @@ import javax.inject.Inject;
 
 /**
  *
- * @author jc.jaramillo10
+ * @author Camilojaravila
  */
 @Stateless
 public class ImagenLogic {
@@ -30,12 +32,12 @@ public class ImagenLogic {
     //private DocumentoLogic DocumentoLogic;
     
     /**
-     * Se encarga de crear un Autor en la base de datos.
+     * Se encarga de crear una Imagen en la base de datos.
      *
-     * @param entity Objeto de AutorEntity con los datos nuevos
-     * @return Objeto de AutorEntity con los datos nuevos y su ID.
+     * @param entity Objeto de ImagenEntity con los datos nuevos
+     * @return Objeto de ImagenEntity con los datos nuevos y su ID.
      */
-    public ImagenEntity createAutor(ImagenEntity entity) {
+    public ImagenEntity createImagen(ImagenEntity entity) {
         LOGGER.log(Level.INFO, "Inicia proceso de crear un autor ");
        
         ImagenEntity buscado = persistence.find(entity.getId());
@@ -54,4 +56,70 @@ public class ImagenLogic {
         
         return null;
     }
+    
+             /**
+     * Obtiene la lista de los registros de Imagenes de un Documento dado.
+     *
+     * @return Colección de objetos de ImagenEntity.
+     */
+    public List<ImagenEntity> getImagenes() {
+        LOGGER.log(Level.INFO, "Inicia proceso de consultar todas las imagenes");
+        return persistence.findAll();
+    }
+    
+     /**
+     * Obtiene los datos de una instancia de un Area de Conocimiento a partir de su ID.
+     *
+     * @param id Identificador de la instancia a consultar
+     * @return Instancia de ImagenEntity con los datos de la Imagen consultada.
+     */
+    public ImagenEntity getImagen(Long id) {
+        LOGGER.log(Level.INFO, "Inicia proceso de consultar una imagen con id = {0}", id);
+        return persistence.find(id);
+    }
+
+    /**
+     * Actualiza la información de una instancia de Imagen.
+     *
+     * @param entity Instancia de ImagenEntity con los nuevos datos.
+     * @return Instancia de ImagenEntity con los datos actualizados.
+     */
+    public ImagenEntity updateImagen(ImagenEntity entity) {
+        LOGGER.log(Level.INFO, "Inicia proceso de actualizar una Imagen ");
+        
+        ImagenEntity buscado = persistence.find(entity.getId());
+        if (buscado != null){
+            LOGGER.log(Level.INFO, "La Imagen con el id {0} no existe ", entity.getId());
+        }
+        else if (!entity.getNombre().matches("([A-Z]|[a-z]|\\s)+")){
+            LOGGER.log(Level.INFO, "El nombre de la Imagen no puede contener caracteres especiales");
+        }
+        else if(entity.getImg().endsWith(".jpg") | entity.getImg().endsWith(".png") | entity.getImg().endsWith(".pdf") | entity.getImg().endsWith(".bmp") | entity.getImg().endsWith(".tif")){
+            LOGGER.log(Level.INFO, "La Imagen tiene un formato no soportado");
+        }
+        else{
+             return persistence.update(entity);
+         }
+        
+        return null;
+    }
+
+    /**
+     * Elimina una instancia de Imagen de la base de datos.
+     *
+     * @param id Identificador de la instancia a eliminar.
+     */
+    public void deleteImagen(Long id) {
+        LOGGER.log(Level.INFO, "Inicia proceso de borrar un area ");
+        
+        ImagenEntity buscado = persistence.find(id);
+        if (buscado != null){
+            LOGGER.log(Level.INFO, "El area de conocimiento con el id {0} no existe ", id);
+        }
+
+        else{
+             persistence.delete(id);
+         }
+    }
+
 }
