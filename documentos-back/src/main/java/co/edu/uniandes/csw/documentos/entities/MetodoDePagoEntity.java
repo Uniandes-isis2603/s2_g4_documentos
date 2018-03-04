b053cce11ea42acb302c477fdd8e1905c9ff1c6f
GREGORIO OSPINA ARANGO <g.ospinaa@sis.virtual.uniandes.edu.co>
2018-03-04 17:35:12 -0500

    Ok, acabe la nueva entidad, persistencia y tests de persistencia de MetodosDePago....

---
+++ documentos-back/src/main/java/co/edu/uniandes/csw/documentos/entities/MetodoDePagoEntity.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.documentos.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import uk.co.jemos.podam.common.PodamExclude;
+
+/**
+ *
+ * @author g.ospinaa
+ */
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public class MetodoDePagoEntity implements Serializable{
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    /**
+     * @return the Id
+     */
+    public Long getId() {
+        return Id;
+    }
+
+    /**
+     * @param Id the Id to set
+     */
+    public void setId(Long Id) {
+        this.Id = Id;
+    }
+}
