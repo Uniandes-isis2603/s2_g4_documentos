8ba7e3614ad6a1a99794610a58c459c98c57f515
vianaernesto <le.viana@uniandes.edu.co>
2018-04-15 05:47:58 -0500

    Avance caso de uso CU_L_001 en el cual se crea un index mas completo

--- documentos-web/src/main/webapp/src/modules/libros/libros.mod.js
+++ documentos-web/src/main/webapp/src/modules/libros/libros.mod.js
@@ -7,7 +7,7 @@
  * configuración del módulo se inyecta la dependencia ui.router que es la que se utiliza para
  * la configuración del las URLs bajo las cuales se accede al módulo.
  * Por ejemplo, para mostrar los libros en la 
- * URL: 'lovalhost:8080/libros/list' es necesario configurar el router por medio
+ * URL: 'localhost:8080/libros/list' es necesario configurar el router por medio
  * del stateProvider que informa a AngulaJS de la relación entre la URL,
  * un estado definido (estado de mostrar libros), el controlador y la vista correspondiente.
  * Los estados definidos en este modulo son:
@@ -22,10 +22,11 @@
  */
 (function(ng) {
     var mod = ng.module("libroModule", ['ui.router']);
-    mod.constant("librosContext", "api/libros");
     mod.config(['$stateProvider','$urlRouterProvider', function ($stateProvider,$urlRouterProvider){
             var basePath = 'src/modules/libros/';
             $urlRouterProvider.otherwise("/librosList");
+            
+            
             $stateProvider.state('libros', {
                 url: '/libros',
                 abstract: true,
