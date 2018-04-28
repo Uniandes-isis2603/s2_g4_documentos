00f56de54a84eec7f82d869ac703bb914e0713bc
N-Sotelo98 <n.sotelo@uniandes.edu.co>
2018-04-27 21:03:47 -0500

    Correcion banner index

--- documentos-web/src/main/webapp/src/modules/Inicio/inicio.ctrl.js
+++ documentos-web/src/main/webapp/src/modules/Inicio/inicio.ctrl.js
@@ -3,6 +3,6 @@
     var mod = ng.module("inicioModule");
 
     mod.controller("inicioCtrl", ['$scope','$rootScope' , '$state', '$stateParams', '$http', function ($scope,$rootScope ,$state, $stateParams, $http) {
-          
+          $rootScope.home=1;
         }]);
 })(window.angular);
--- documentos-web/src/main/webapp/src/modules/cursos/cursos.ctrl.js
+++ documentos-web/src/main/webapp/src/modules/cursos/cursos.ctrl.js
@@ -2,8 +2,8 @@
 
     var mod = ng.module("cursosModule");
 
-    mod.controller("cursosCtrl", ['$scope', '$state', '$stateParams', '$http', 'citiesContext', function ($scope, $state, $stateParams, $http, context) {
-
+    mod.controller("cursosCtrl", ['$scope', '$rootScope','$state', '$stateParams', '$http', 'citiesContext', function ($scope, $rootScope,$state, $stateParams, $http, context) {
+            $rootScope.home=1;
             $scope.records = {};
             $http.get("http://localhost:8080/documentos-web/api/cursos").then(function (response) 
             {
