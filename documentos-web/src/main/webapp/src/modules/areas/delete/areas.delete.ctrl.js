e5c0e391bc1493af1540b21c6153062fc81b3a7b
jc.jaramillo10 <jc.jaramillo10@ISIS2603S2-0072.sis.virtual.uniandes.edu.co>
2018-04-28 21:10:31 -0500

    Se terminan los casos CU_A_001, CU_A_002, CU_AC_001, CU_AC_002. Tambien se arreglaron unos bugs en el back, Se realizan avances en los casos CU_A_003, CU_A_004, CU_A_005, CU_AC_003, CU_AC_004, CU_AC_005
    Segunda vez

---
+++ documentos-web/src/main/webapp/src/modules/areas/delete/areas.delete.ctrl.js
@@ -0,0 +1,38 @@
+(function (ng) {
+    var mod = ng.module("areasModule");
+    mod.constant("areaContext", "api/areas");
+    mod.controller('areaDeleteCtrl', ['$scope', '$http', 'areaContext', '$state',
+        /**
+         * @ngdoc controller
+         * @name autor.controller:areaDeleteCtrl
+         * @description
+         * Definición del controlador auxiliar para eliminar Areas. 
+         * @param {Object} $scope Referencia injectada al Scope definida para este
+         * controlador, el scope es el objeto que contiene las variables o 
+         * funciones que se definen en este controlador y que son utilizadas 
+         * desde el HTML.
+         * @param {Object} $http Objeto injectado para la manejar consultas HTTP
+         * @param {Object} areaContext Constante injectada que contiene la ruta
+         * donde se encuentra el API de Autores en el Backend.
+         * @param {Object} $state Dependencia injectada en la que se recibe el 
+         * estado actual de la navegación definida en el módulo.
+         */
+        function ($scope, $http, areaContext, $state) {
+            var idArea = $state.params.autorId;
+            /**
+             * @ngdoc function
+             * @name deleteArea
+             * @methodOf area.controller:areaDeleteCtrl
+             * @description
+             * Esta función utiliza el protocolo HTTP para eliminar el area.
+             * @param {String} id El ID del area a eliminar.
+             */
+            $scope.deleteAutor = function () {
+                $http.delete(areaContext + '/' + idArea, {}).then(function (response) {
+                    $state.go('areasList', {areaId: response.data.id}, {reload: true});
+                });
+            };
+        }
+    ]);
+}
+)(window.angular);
