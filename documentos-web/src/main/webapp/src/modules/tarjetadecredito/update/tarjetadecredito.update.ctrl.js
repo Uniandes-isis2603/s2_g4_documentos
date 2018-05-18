60d9744eda06bcae866cfef0cee6ed2da52fc55a
nicolassotelo <nicolassotelo@157.253.220.248>
2018-05-17 22:49:08 -0500

    Impelmentacion de los detailed de cusrsos , areas , autores y areaDeConocimiento

--- documentos-web/src/main/webapp/src/modules/tarjetadecredito/update/tarjetadecredito.update.ctrl.js
+++ documentos-web/src/main/webapp/src/modules/tarjetadecredito/update/tarjetadecredito.update.ctrl.js
@@ -4,21 +4,23 @@
     mod.controller('tarjetadecreditoUpdateCtrl', ['$scope', '$http', 'tarjetadecreditoContext', '$state', '$rootScope',
         function ($scope, $http, tarjetadecreditoContext, $state, $rootScope) {
 
+            $rootScope.edit = true;
+            
             $scope.data = {};
 
             var tarjetadecreditoId = $state.params.tarjetadecreditoId;
 
             $http.get(tarjetadecreditoContext + '/' + tarjetadecreditoId).then(function (response) {
                 var tarjetadecredito = response.data;
-                $scope.data.nroEnLaTarjeta = tarjetadecredito.nroEnLaTarjeta;
+                $scope.data.nroDeLaTarjeta = tarjetadecredito.nroDeLaTarjeta;
                 $scope.data.nombreEnLaTarjeta = tarjetadecredito.nombreEnLaTarjeta;
                 $scope.data.numeroDeSeguridad = tarjetadecredito.numeroDeSeguridad;
-                $scope.data.tipoDeLaTarjeta = tarjetadecredito.numeroDeSeguridad;
+                $scope.data.tipoDeTarjeta = tarjetadecredito.tipoDeTarjeta;
 
             });
 
             $scope.createTarjetadecredito = function () {
-                $http.put(tarjetadecreditoContext + "/" + tarjetadecreditoId, $scope.data).then(function (response) {
+                $http.put(tarjetadecreditoContext + '/' + tarjetadecreditoId, $scope.data).then(function (response) {
                     $state.reload();
                 });
             };
