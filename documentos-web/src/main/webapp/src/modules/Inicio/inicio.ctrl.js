dcd037eec91b19185edeb115fe2dbe952539a0b8
gregorioospina <gregorioospina@hotmail.com>
2018-05-17 22:01:59 -0500

    viejo

--- documentos-web/src/main/webapp/src/modules/Inicio/inicio.ctrl.js
+++
@@ -1,8 +0,0 @@
-(function (ng) {
-
-    var mod = ng.module("inicioModule");
-
-    mod.controller("inicioCtrl", ['$scope', '$state', '$stateParams', '$http', function ($scope,$state, $stateParams, $http) {
-          sessionStorage.getItem("home");
-        }]);
-})(window.angular);
